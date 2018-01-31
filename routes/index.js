4b6b0059952e9e86dcab7b0aabf0f11ebb5c656a
manja <anjiemerchant@gmail.com>
2018-01-31 12:57:07 -0500

    final commit

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const Hotel = require('../server/models').Hotel;
 const Restaurant = require('../server/models').Restaurant;
 const Activity = require('../server/models').Activity;
 
-router.get ("/api", (req, res, next) => {
+router.get("/api", (req, res, next) => {
     const allHotels = Hotel.findAll({include: {model:Place} })
     const allRestaurants = Restaurant.findAll({include: {model:Place} })
     const allActivity = Activity.findAll({include: {model:Place} });
